d6c112daffe4838b50ba5ee2b5543ae2e3646d53
nineya <361654768@qq.com>
2021-04-18 01:28:04 +0800

    104. 二叉树的最大深度

---
+++ leetcode-algo/src/main/java/com/nineya/algo/leetcode/algorithms/Solution_104.java
@@ -0,0 +1,36 @@
+package com.nineya.algo.leetcode.algorithms;
+
+import com.nineya.algo.leetcode.algorithms.assist.TreeNode;
+
+/**
+ * @author 殇雪话诀别
+ * 2021/4/18
+ */
+
+public class Solution_104 {
+    private int doMaxDepth(TreeNode node, int depth) {
+        if (node.right == null && node.left == null) {
+            return depth;
+        }
+        depth++;
+        if (node.left != null && node.right != null) {
+            return Math.max(doMaxDepth(node.left, depth), doMaxDepth(node.right, depth));
+        }
+        if (node.right != null) {
+            return doMaxDepth(node.right, depth);
+        }
+        return doMaxDepth(node.left, depth);
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return doMaxDepth(root, 1);
+    }
+
+    public static void main(String[] args) {
+        Solution_104 solution = new Solution_104();
+        System.out.println(solution.maxDepth(new TreeNode(1, new TreeNode(2), new TreeNode(3))));
+    }
+}
