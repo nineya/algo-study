623ec8b9fd1ad1d9da86702c5276f93fd1f5c8a8
nineya <361654768@qq.com>
2021-04-18 00:42:08 +0800

    83. 删除排序链表中的重复元素

---
+++ leetcode-algo/src/main/java/com/nineya/algo/leetcode/algorithms/Solution_83.java
@@ -0,0 +1,39 @@
+package com.nineya.algo.leetcode.algorithms;
+
+import com.nineya.algo.leetcode.algorithms.assist.ListNode;
+
+/**
+ * @author 殇雪话诀别
+ * 2021/4/18
+ */
+
+public class Solution_83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        int n = head.val;
+        ListNode node = head;
+        ListNode node2 = head;
+        while (node != null) {
+            if (node.val != n) {
+                node2.next = node;
+                node2 = node;
+                n = node.val;
+            }
+            node = node.next;
+        }
+        node2.next = null;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Solution_83 solution = new Solution_83();
+        ListNode n1 = new ListNode(1, new ListNode(1, new ListNode(1)));
+        ListNode n = solution.deleteDuplicates(n1);
+        while (n != null) {
+            System.out.print(n.val + " ");
+            n = n.next;
+        }
+    }
+}
