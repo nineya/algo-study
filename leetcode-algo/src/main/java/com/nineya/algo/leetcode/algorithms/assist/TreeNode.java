decd8932e190de094169fcf63582c2de66124045
nineya <361654768@qq.com>
2021-04-18 01:11:01 +0800

    100. 相同的树

---
+++ leetcode-algo/src/main/java/com/nineya/algo/leetcode/algorithms/assist/TreeNode.java
@@ -0,0 +1,24 @@
+package com.nineya.algo.leetcode.algorithms.assist;
+
+/**
+ * @author 殇雪话诀别
+ * 2021/4/18
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
